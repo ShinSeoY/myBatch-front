2590cd3daf570d3c46e9a9208e5aca0351ff13c1
ShinSeoY <tjdudtls312@gmail.com>
2024-03-01 18:50:48 +0900

    - 기본 테이블 설정

--- src/pages/Bookmarks.vue
+++ src/pages/Bookmarks.vue
@@ -1,14 +1,92 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const columns:any = [
+  {
+    name: 'index',
+    label: '#',
+    field: 'index'
+  },
+  {
+    name: 'name',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: (row:any) => row.name,
+    format: (val: any) => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a:any, b:any) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a:any, b:any) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const seed = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  }
+]
+
+// we generate lots of rows here
+let rows:any = []
+for (let i = 0; i < 1000; i++) {
+  rows = rows.concat(seed.slice(0).map(r => ({ ...r })))
+}
+rows.forEach((row:any, index:any) => {
+  row.index = index
+})
+
+    const  pagination = ref({
+        rowsPerPage: 0
+      })
 
 </script>
 
 <template>
   <div id="app">
     <q-page class="q-pa-md">
-      <h1>나의 관심 환율</h1>
-      <p v-for="n in 15" :key="n">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
-      </p>
+      <q-table
+      style="height: 400px"
+      flat bordered
+      title="내 관심 환율"
+      :rows="rows"
+      :columns="columns"
+      row-key="index"
+      virtual-scroll
+      v-model:pagination="pagination"
+      :rows-per-page-options="[0]"
+    />
     </q-page>
   </div>
 </template>
--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -1,12 +1,82 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const columns:any = [
+  {
+    name: 'desc',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: (row:any) => row.name,
+    format: (val:any) => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a:any, b:any) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a:any, b:any) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows:any = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  }
+]
+
+const selected = ref([])
+const getSelectedString = () => {
+    return selected.value.length === 0 ? '' : `${selected.value.length} record${selected.value.length > 1 ? 's' : ''} selected of ${rows.length}`
+  }
 </script>
 
 <template>
   <div id="app">
     <q-page class="q-pa-md">
       <h1>계산기</h1>
-      <h1></h1>
+      <q-table
+      flat bordered
+      title="오늘의 환율"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :selected-rows-label="getSelectedString"
+      selection="multiple"
+      v-model:selected="selected"
+    />
+
+    <div class="q-mt-md">
+      Selected: {{ JSON.stringify(selected) }}
+    </div>
     </q-page>
   </div>
 </template>
