aa6a1fb644b419b6fc6742b1abe0b601b735c10b
ShinSeoY <tjdudtls312@gmail.com>
2024-04-11 12:11:49 +0900

    - 배너 추가

--- src/layouts/CommonLayout.vue
+++ src/layouts/CommonLayout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { url } from 'inspector'
 import { ref } from 'vue'
 
 const tab = ref('bookmarks')
@@ -8,17 +9,11 @@ const tab = ref('bookmarks')
   <div class="q-pa-md">
     <q-layout view="lHh lpr lFf" container style="height: 800px" class="shadow-2 rounded-borders">
       <q-header elevated class="my-q-header">
-        <!-- <q-toolbar> -->
-        <!-- <q-btn flat round dense icon="menu" class="q-mr-sm" /> -->
-        <!-- <q-toolbar-title> </q-toolbar-title> -->
-
-        <!-- <q-btn flat round dense icon="whatshot" /> -->
-        <!-- </q-toolbar> -->
-
+        <q-img src="../assets/banner.png" style="height: 140px"></q-img>
         <q-tabs v-model="tab">
-          <q-route-tab to="/bookmarks" name="bookmarks" label="관심 환율" />
-          <q-route-tab to="/calculator" name="calculator" label="환율 계산기" />
-          <q-route-tab to="/notification" name="notification" label="알림 서비스" />
+          <q-route-tab class="tab" to="/bookmarks" name="bookmarks" label="관심 환율" />
+          <q-route-tab class="tab" to="/calculator" name="calculator" label="환율 계산기" />
+          <q-route-tab class="tab" to="/notification" name="notification" label="알림 서비스" />
         </q-tabs>
       </q-header>
 
@@ -32,5 +27,8 @@ const tab = ref('bookmarks')
 <style scoped>
 .my-q-header {
   padding-top: 10px;
+}
+.tab {
+  width: 150px;
 }
 </style>
